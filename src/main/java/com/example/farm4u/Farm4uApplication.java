f7aa67603a1b700496f20692cb14103513f1596f
jiwoo_nyang <jweun1@soongsil.ac.kr>
2025-07-17 00:16:25 +0900

    [test] git commit test

---
+++ src/main/java/com/example/farm4u/Farm4uApplication.java
@@ -0,0 +1,13 @@
+package com.example.farm4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farm4uApplication.class, args);
+	}
+
+}
